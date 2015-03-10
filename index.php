c7065bc077465977a90cfa5650576aa6b13d8923
vincentatpdata <vincent@pdata.no>
2015-03-10 14:40:14 +0100

    Testing the new branch

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 	<h1>Committing an edit to a file</h1>
 	<p>Adding a line from another computer.</p>
 	<p>And this is another line.</p>
+	<p>Is this commited to the april15 branch?</p>
 </body>
 </html>
