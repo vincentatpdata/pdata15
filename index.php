d7c0656dd5ed991dc3740ad57d351e1f5bb949ab
vincentatpdata <vincent@pdata.no>
2015-03-10 14:47:14 +0100

    Removed a line.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <body>
 	<h1>Committing an edit to a file</h1>
 	<p>Adding a line from another computer.</p>
-	<p>And this is another line.</p>
 	<p>Is this commited to April15?</p>
 </body>
 </html>
