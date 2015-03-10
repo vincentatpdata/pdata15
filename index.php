24c6e6a784815fbf57837979cce686d325d7436c
vincentatpdata <vincent@pdata.no>
2015-03-10 14:51:29 +0100

    Cleaned up.

--- index.php
+++ index.php
@@ -4,13 +4,7 @@
 	<title>Test</title>
 </head>
 <body>
-	<h1>Committing an edit to a file</h1>
-	<p>Adding a line from another computer.</p>
-<<<<<<< HEAD
-	<p>Is this commited to April15?</p>
-=======
-	<p>And this is another line.</p>
-	<p>Is this commited to the april15 branch?</p>
->>>>>>> c7065bc077465977a90cfa5650576aa6b13d8923
+	<h1>Commiting an edit to a file</h1>
+	<p>Cleaned up clutter</p>
 </body>
 </html>
